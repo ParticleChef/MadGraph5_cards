69b5a62e77a5d42688027aca6be402f0a70adaf3
ParticleChef <demonichje@gmail.com>
2021-03-17 17:14:04 +0900

    new signal mass and modified the make script

--- MonoTop/FCNC/makeModel.sh
+++ MonoTop/FCNC/makeModel.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-for Med in 1700
+for Med in 1750
 do
     echo ${Med}
-    DM=800
+    DM=700
     modelName=DMsimp_s_spin1_monotop_${Med}_${DM}_800
     echo ${modelName}
+    cd MonotopModels_add
     cp -r DMsimp_s_spin1_monotop_1500_150_800 DMsimp_s_spin1_monotop_${Med}_${DM}_800
+    ls ${modelName}
 
     cd ${modelName}/
+    ls
+    echo change the Med parameter.
     sed -i "296s/.*/                 value =  "${Med}",/g" parameters.py
 
     cd ../
+    ls
+    cd ..
 
 done
