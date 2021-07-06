fdae3dca91c1a2c6687634a4bf1c1692de9b8d2a
ParticleChef <demonichje@gmail.com>
2021-07-06 14:50:55 +0900

    Full version of reweight card

--- MonoTop/FCNC/CardMake_ULrwgt.sh
+++ MonoTop/FCNC/CardMake_ULrwgt.sh
@@ -4,7 +4,7 @@ for Med in 195 200 295 300 495 500 750 995 1000 1250 1495 1500 1700 1750 1995 20
 do
     for DM in 50 100 150 250 300 500 700 750 800 1000 1250 1500 2000
     do
-        cardDir=MonotopCards_New
+        cardDir=MonotopCards_ULrwgt_full
         copyDir=sebastian_cards
         copyMass=Vector_MonoTop_NLO_Mphi-2500_Mchi-750_gSM-0p25_gDM-1p0_13TeV-madgraph
         targetDir=Vector_MonoTop_NLO_Mphi-${Med}_Mchi-${DM}_gSM-0p25_gDM-1p0_13TeV-madgraph
@@ -19,7 +19,8 @@ do
             cp ${copyDir}/${copyMass}_customizecards.dat ${cardDir}/${targetDir}/${targetDir}_customizecards.dat
             cp ${copyDir}/${copyMass}_extramodels.dat ${cardDir}/${targetDir}/${targetDir}_extramodels.dat
             cp ${copyDir}/${copyMass}_madspin_card.dat ${cardDir}/${targetDir}/${targetDir}_madspin_card.dat
-            cp ${copyDir}/${copyMass}_reweight_card.dat ${cardDir}/${targetDir}/${targetDir}_reweight_card.dat
+            #cp ${copyDir}/${copyMass}_reweight_card.dat ${cardDir}/${targetDir}/${targetDir}_reweight_card.dat
+            cp ${copyDir}/${copyMass}_reweight_card_full.dat ${cardDir}/${targetDir}/${targetDir}_reweight_card.dat
 
             if [ -e ${cardDir}/${targetDir}/${targetDir}_proc_card.dat ]; then
                 cd ${cardDir}/${targetDir}/
